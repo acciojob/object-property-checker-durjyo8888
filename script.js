7e8381010ea5defc9d003f958376760f5506dc7c
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-10-26 21:18:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	return sampleObject.hasOwnProperty(key);
 }
-
+// console.log(hasKey("red"));    // true
+// console.log(hasKey("blue"));   // false
+// console.log(hasKey("white"));  // true
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
